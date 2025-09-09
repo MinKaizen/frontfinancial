49a8be8a80463dd36e49deecb1a038989c38285c
minkaizen <martin.g.cao@gmail.com>
2025-09-09 15:08:28 +1000

    feat: add home footer section

---
+++ app/footer-section.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export function FooterSection() {
+  return (
+    <div className="grid justify-items-center py-5 px-5 h-[600px] grid-rows-[auto_1fr_auto] items-center">
+      <p className="text-right leading-none justify-self-end">Sharp-minded, never-sleeping, tech-driven <br className="hiddem md:inline-block" /> financial partners who make your money <br className="hidden md:inline-block" />work as hard as we do.</p>
+      <Image
+        className="w-full max-w-[331px] h-auto"
+        src="/frontfinancial-logo-script-tan.svg"
+        alt="Front Financial logo"
+        width={331}
+        height={53}
+        priority
+      />
+      <div className="grid grid-cols-[auto_auto_1fr] justify-self-stretch gap-x-20">
+        <div className="flex flex-col">
+          <Link href="/">Instagram</Link>
+          <Link href="/">LinkedIn</Link>
+        </div>
+        <div className="flex flex-col">
+          <Link href="/">Privacy</Link>
+          <Link href="/">FAQ's</Link>
+          <Link href="/">T&C's</Link>
+        </div>
+        <div className="justify-self-end self-end">
+          <div>Front Financial 2025©</div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
 import { HeroSection } from "./hero-section";
 import { ContactSection } from "./contact-section";
+import { FooterSection } from "./footer-section";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <ContactSection />
+      <FooterSection />
     </>
   );
 }
