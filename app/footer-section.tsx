ddde1ffe98d68dfcaa8ef70e2cdb70fe12648fed
minkaizen <martin.g.cao@gmail.com>
2025-09-09 16:18:37 +1000

    feat: update footer social links

--- app/footer-section.tsx
+++ app/footer-section.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 export function FooterSection() {
+  const instagramUrl = process.env.INSTAGRAM_URL ?? '';
+  const linkedInUrl = process.env.LINKED_IN_URL ?? '';
+
   return (
     <div className="grid justify-items-center py-10 px-5 h-[600px] grid-rows-[auto_1fr_auto] items-center">
       <p className="text-right leading-none justify-self-end">Sharp-minded, never-sleeping, tech-driven <br className="hidden sm:inline-block" /> financial partners who make your money <br className="hidden sm:inline-block" />work as hard as we do.</p>
@@ -15,8 +18,8 @@ export function FooterSection() {
       />
       <div className="grid sm:grid-cols-[auto_auto_1fr] justify-self-stretch gap-x-20">
         <div className="flex flex-col">
-          <Link href="/">Instagram</Link>
-          <Link href="/">LinkedIn</Link>
+          <Link href={instagramUrl}>Instagram</Link>
+          <Link href={linkedInUrl}>LinkedIn</Link>
         </div>
         <div className="flex flex-col">
           <Link href="/">Privacy</Link>
