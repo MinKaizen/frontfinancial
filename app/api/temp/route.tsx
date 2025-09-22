743fd691cf936e19f7fcb76c0199030ff92f6960
minkaizen <martin.g.cao@gmail.com>
2025-09-22 10:35:33 +1000

    feat: temporary temp route

---
+++ app/api/temp/route.tsx
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const portalId = process.env.HUBSPOT_PORTAL_ID!;
+  const formGuid = process.env.HUBSPOT_EMAIL_FORM_GUID!;
+  const token = process.env.HUBSPOT_ACCESS_TOKEN;
+  const hasHsToken = Boolean(token)
+
+  const data = {
+    portalId,
+    formGuid,
+    hasHsToken,
+  }
+
+  return NextResponse.json(data, { status: 200 });
+}
