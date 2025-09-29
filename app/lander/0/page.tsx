b037b7a8a5d3006881fcee21ba82f2d47e272da7
Martin <martin.cao@hustlrmedia.com>
2025-09-30 02:29:31 +1000

    feat: copy temp lander to route /lander/0

---
+++ app/lander/0/page.tsx
@@ -0,0 +1,13 @@
+import { HeroSection } from "@/components/home-page/hero-section";
+import { ContactSection } from "@/components/home-page/contact-section";
+import { FooterSection } from "@/components/home-page/footer-section";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <ContactSection />
+      <FooterSection />
+    </>
+  );
+}
