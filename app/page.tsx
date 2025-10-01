d07315f8840d433d50083354083aee068f2049fe
Martin <martin.cao@hustlrmedia.com>
2025-10-01 13:16:09 +1000

    feat: use new sections in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import { HeroSection } from "@/components/home-page/hero-section";
+import { AboutSection } from "@/components/home-page/about-section";
+import { ServiceSection } from "@/components/home-page/service-section";
+import { ServiceLogosSection } from "@/components/home-page/service-logos-section";
 import { ContactSection } from "@/components/home-page/contact-section";
+import { TestimonialSection } from "@/components/home-page/testimonial-section";
 import { FooterSection } from "@/components/home-page/footer-section";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <AboutSection />
+      <ServiceSection />
+      <ServiceLogosSection />
       <ContactSection />
+      <TestimonialSection />
       <FooterSection />
     </>
   );
