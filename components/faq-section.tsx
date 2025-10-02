f4ea3d9dce6d784ef0d73e95b97b7f22a7f92623
minkaizen <martin.g.cao@gmail.com>
2025-10-03 09:58:02 +1000

    feat: add services page faq section

---
+++ components/faq-section.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export function FaqSection() {
+  return (
+    <div className="bg-white text-charcoal">
+      <div className="flex flex-col-reverse lg:grid grid-cols-2">
+        <div className="px-5 py-8 lg:py-12 xl:py-18 lg:grid grid-rows-[auto_1fr_1fr]">
+          <h2 className="pb-12 text-body">Frequently asked questions.</h2>
+          <div className="text-[14px]">
+            <p className="">Truth be told, the most common question we get is <br />"When do we start?"</p>
+            <p className="mt-[1.5em]">Luckily for you, the answer lives on our contact page</p>
+          </div>
+          <Link href="/contact" className="block w-max relative cursor-pointer mt-[45px] lg:mt-5 xl:mt-[45px]">
+            <div className="pr-3 text-[14px]">Explore</div>
+            <div
+              className="arrow absolute left-0 bottom-3"
+              style={{
+                ["--len" as any]: "100%",
+                ["--thick" as any]: "1px",
+                ["--head" as any]: "4px",
+                color: "#393838",
+              }}
+            />
+          </Link>
+        </div>
+        <div className="aspect-[960/648] w-full">
+          <Image
+            className="object-center object-cover h-full w-full"
+            src="/old-building.avif"
+            alt="Old building"
+            height={648}
+            width={960}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
