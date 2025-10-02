75a98c7c62d6a5306c6e0e801325e5cb0114fe28
minkaizen <martin.g.cao@gmail.com>
2025-10-03 09:57:45 +1000

    feat: copy testimonial section from home page to global component

---
+++ components/testimonial-section.tsx
@@ -0,0 +1,14 @@
+import TestimonialCarousel from "./testimonial-carousel";
+
+export function TestimonialSection() {
+  return (
+    <div className="bg-tan text-white text-[18px]">
+      <div className="py-20 md:py-30 lg:py-48 px-5">
+        <div className="max-w-[480px] mx-auto">
+          <div className="text-center mb-[1em]">They said it:</div>
+          <TestimonialCarousel />
+        </div>
+      </div>
+    </div>
+  );
+}
