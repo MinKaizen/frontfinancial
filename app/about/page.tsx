d7b6b690381bb60ca42b3a4cef36136c34b82fbd
minkaizen <martin.g.cao@gmail.com>
2025-10-03 10:24:45 +1000

    feat: add bare about page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/header"
+import { FooterSection } from "@/components/footer-section"
+
+export default function Contact() {
+  return (
+    <>
+      <Header />
+      <FooterSection />
+    </>
+  );
+}
