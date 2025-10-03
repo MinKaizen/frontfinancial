4c1ad496f99a4319e4cfe28907af8e50c6180805
minkaizen <martin.g.cao@gmail.com>
2025-10-03 12:37:02 +1000

    feat: support passing bg and text color to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,18 @@
 import Link from "next/link"
 import Image from "next/image"
 
-export function Header() {
+type Props = {
+  bgColor?: string,
+  textColor?: string,
+}
+
+export function Header({bgColor = "transparent", textColor = "white"}: Props) {
+  const bgClass = `bg-${bgColor}`
+  const textClass = `text-${textColor}`
+  const containerClass = `${bgClass} ${textClass}`
+
   return (
-    <>
+    <div className={containerClass}>
       <div className="grid grid-cols-[1fr_auto_1fr] px-5 py-7">
         <div className="flex flex-row gap-12 text-[18px]">
           <Link href="/">Home</Link>
@@ -13,7 +22,7 @@ export function Header() {
         <Link href="/" className="grid place-items-center">
           <Image
             className="w-full max-w-[220px] h-auto"
-            src="/frontfinancial-logo-primary-white.svg"
+            src={ `/frontfinancial-logo-primary-${textColor}.svg` }
             alt="Next.js logo"
             width={220}
             height={15}
@@ -24,6 +33,6 @@ export function Header() {
           <Link href="/contact">Contact</Link>
         </div>
       </div>
-    </>
+    </div>
   )
 }
