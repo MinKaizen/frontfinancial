a245bb95e37f552704c1f91a4821b0ea469fae17
minkaizen <martin.g.cao@gmail.com>
2025-10-03 14:39:03 +1000

    feat: use global header on home hero section

--- components/home-page/hero-section.tsx
+++ components/home-page/hero-section.tsx
@@ -1,18 +1,13 @@
 import Image from "next/image";
+import { Header } from "@/components/header";
 
 export function HeroSection() {
   return (
-    <div className="grid justify-items-center py-15 px-10 relative min-h-[100vh] grid-rows-[auto_1fr_auto] items-center bg-[url('/home-hero-image.png')] bg-cover bg-center bg-no-repeat">
-      <Image
-        className="w-full max-w-[220px] h-auto"
-        src="/frontfinancial-logo-primary-white.svg"
-        alt="Next.js logo"
-        width={220}
-        height={15}
-        priority
-      />
+    <div className="grid justify-items-center py-15 px-10 relative min-h-[100vh] place-items-center bg-[url('/home-hero-image.png')] bg-cover bg-center bg-no-repeat">
+      <div className="absolute left-0 top-0 w-full">
+        <Header></Header>
+      </div>
       <p className="uppercase text-sm md:text-base lg:text-lg font-heading text-center">sharp-minded, never-sleeping, tech-driven financial partners</p>
-      <div></div>
     </div>
   );
 }
