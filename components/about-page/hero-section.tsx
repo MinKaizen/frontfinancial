f5a10b5036d1a3e0f09009c916cc6bca79a849e8
minkaizen <martin.g.cao@gmail.com>
2025-10-03 16:08:20 +1000

    feat: adjust height of about hero section

--- components/about-page/hero-section.tsx
+++ components/about-page/hero-section.tsx
@@ -5,7 +5,7 @@ export function HeroSection() {
 
   return (
     <div style={style} className="bg-center bg-cover text-white">
-      <div className="h-[400px] md:h-[577px] grid place-items-center">
+      <div className="h-[400px] md:h-[435px] grid place-items-center">
         <div>
           <h1 className="text-center text-body leading-tight">We're front-facing</h1>
           <p className="text-center text-body leading-tight font-heading">Ready to tell you when your finances are going well... <br className="hidden sm:block" />or very well.</p>
