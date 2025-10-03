5252af83c9fd66233688b1cc20ed44fe346352ab
minkaizen <martin.g.cao@gmail.com>
2025-10-03 10:58:37 +1000

    feat: add about page hero section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/header"
+import { HeroSection } from "@/components/about-page/hero-section"
 import { FooterSection } from "@/components/footer-section"
 
 export default function Contact() {
   return (
-    <>
+    <div className="bg-white">
       <Header />
+      <HeroSection />
       <FooterSection />
-    </>
+    </div>
   );
 }
---
+++ components/about-page/hero-section.tsx
@@ -0,0 +1,16 @@
+export function HeroSection() {
+  const style = {
+    backgroundImage: `url("/sydney-sunset.avif")`
+  }
+
+  return (
+    <div style={style} className="bg-center bg-cover text-white">
+      <div className="h-[400px] md:h-[577px] grid place-items-center">
+        <div>
+          <h1 className="text-center text-body leading-tight">We're front-facing</h1>
+          <p className="text-center text-body leading-tight font-heading">Ready to tell you when your finances are going well... <br className="hidden sm:block" />or very well.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
