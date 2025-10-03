13486429da8c4d024cee56ae1db1240fb2e903fe
minkaizen <martin.g.cao@gmail.com>
2025-10-03 11:24:15 +1000

    feat: add about page process section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@/components/header"
 import { HeroSection } from "@/components/about-page/hero-section"
 import { AboutSection } from "@/components/about-page/about-section"
+import { ProcessSection } from "@/components/about-page/process-section"
 import { FooterSection } from "@/components/footer-section"
 
 export default function Contact() {
@@ -9,6 +10,7 @@ export default function Contact() {
       <Header />
       <HeroSection />
       <AboutSection />
+      <ProcessSection />
       <FooterSection />
     </div>
   );
