6073e03d05fbad7b011f06f549fccee17d66a90f
minkaizen <martin.g.cao@gmail.com>
2025-10-21 18:54:28 +1100

    feat: use new process images in about process section

--- components/about-page/process-section.tsx
+++ components/about-page/process-section.tsx
@@ -28,27 +28,27 @@ export function ProcessSection() {
         <h2 className="text-body font-heading">Our process</h2>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 mt-10">
           <ProcessCard
-            img="/our-process1.avif"
+            img="/process1.avif"
             title="One. Consultation & Understanding"
             description="We define your goals with precision: focused, actionable, and designed to deliver."
           />
           <ProcessCard
-            img="/our-process2.avif"
+            img="/process2.avif"
             title="Two. Research & Analysis"
             description="We do all the due diligence on your situation, analyse it from multiple angles, review our internal funding structures, and uncover the smartest opportunities available."
           />
           <ProcessCard
-            img="/our-process3.avif"
+            img="/process3.avif"
             title="Three. Presenting Options."
             description="Only the best options. Expertly selected. Positioned for maximum advantage."
           />
           <ProcessCard
-            img="/our-process4.avif"
+            img="/process4.avif"
             title="Four. Execution & Settlement."
             description="Applications, approvals, and settlement handled flawlessly. Every detail managed to perfection."
           />
           <ProcessCard
-            img="/our-process5.avif"
+            img="/process5.avif"
             title="Five. Post-Settlement Support."
             description="Our commitment extends beyond settlement. We provide ongoing insight and strategy to protect and grow your position."
           />
