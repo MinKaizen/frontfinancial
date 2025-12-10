636d8286db1f7741a4aae1c8646553dea3c4b031
minkaizen <martin.g.cao@gmail.com>
2025-12-10 13:21:55 +1100

    feat: implement faq-process-section

--- app/property-equity-lander/faq-process-section.tsx
+++ app/property-equity-lander/faq-process-section.tsx
@@ -1,8 +1,87 @@
+"use client"
+
+import { Faq } from "./faq";
+import { ArrowDown, ArrowRight, ArrowLeft } from "lucide-react";
+
+type StepProps = {
+  title: string,
+  description: string,
+}
+
+const StepCard = (props: { step: StepProps }) => {
+  return (
+    <article className="grid justify-items-center bg-[#666573] w-full max-w-[180px] h-[200px] rounded-2xl py-5 px-5">
+      <h3 className="text-sm text-white">{props.step.title}</h3>
+      <div className="text-sm text-white" dangerouslySetInnerHTML={{ __html: props.step.description }}></div>
+    </article>
+  )
+}
+
+const LongArrowRight = ({ className }: { className?: string }) => {
+  return (
+    <svg width="32" height="6" viewBox="0 0 32 6" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
+      <path d="M32 2.88675L27 -2.14577e-06V5.7735L32 2.88675ZM0 2.88675V3.38675H27.5V2.88675V2.38675H0V2.88675Z" fill="#B2AEA7" />
+    </svg>
+  );
+};
+
+const LongArrowDown = ({ className }: { className?: string }) => {
+  return (
+    <svg width="7" height="32" viewBox="0 0 6 32" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
+      <path d="M2.88672 32L5.77347 27L-3.38436e-05 27L2.88672 32ZM2.88672 0L2.38672 -2.18557e-08L2.38672 27.5L2.88672 27.5L3.38672 27.5L3.38672 2.18557e-08L2.88672 0Z" fill="#B2AEA7" />
+    </svg>
+  );
+};
+const steps: StepProps[] = [
+  {
+    title: `Step 1`,
+    description: `Fill out inquiry form`
+  },
+  {
+    title: `Step 2`,
+    description: `A. Introduction 15 Minute Call<br>B. Fill in express form`
+  },
+  {
+    title: `Step 3`,
+    description: `Proposal and Approval`
+  },
+  {
+    title: `Step 1`,
+    description: `Discovery Call`
+  },
+]
 
 export function FaqProcessSection() {
   return (
-    <>
-
-    </>
+    <div className="bg-tan-light text-black">
+      <div className="grid lg:grid-cols-2">
+        <div className="px-5 py-8 lg:py-12 xl:py-18">
+          <h2 className="mb-7 text-body">Frequently asked questions.</h2>
+          <Faq />
+        </div>
+        <div className="w-full grid place-items-center px-4 py-14 bg-[#3F404F]">
+          <div className="grid gap-1 md:gap-3 grid-cols-[1fr_auto_1fr] items-center content-center w-auto justify-center justify-items-center">
+            <StepCard step={steps[0]} />
+            <div>
+              <LongArrowRight className="hidden md:block" />
+              <ArrowRight className="md:hidden text-tan" />
+            </div>
+            <StepCard step={steps[1]} />
+            <div></div>
+            <div></div>
+            <div>
+              <LongArrowDown className="hidden md:block" />
+              <ArrowDown className="md:hidden text-tan" />
+            </div>
+            <StepCard step={steps[3]} />
+            <div>
+              <LongArrowRight className="hidden md:block rotate-180" />
+              <ArrowLeft className="md:hidden text-tan" />
+            </div>
+            <StepCard step={steps[2]} />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
---
+++ app/property-equity-lander/faq.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { ChevronDown } from 'lucide-react';
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+type Faq = {
+  question: string,
+  answer: string,
+}
+
+const faqs: Faq[] = [
+  {
+    question: `What exactly is "using my property equity" to access funds?`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at urna pellentesque, vehicula urna quis, imperdiet libero. Aenean maximus sem id tempor accumsan. Integer nec ligula laoreet, blandit leo et, fermentum turpis.`,
+  },
+  {
+    question: `How do I know if I qualify?`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at urna pellentesque, vehicula urna quis, imperdiet libero. Aenean maximus sem id tempor accumsan. Integer nec ligula laoreet, blandit leo et, fermentum turpis.`,
+  },
+  {
+    question: `How fast can you get me approved?`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at urna pellentesque, vehicula urna quis, imperdiet libero. Aenean maximus sem id tempor accumsan. Integer nec ligula laoreet, blandit leo et, fermentum turpis.`,
+  },
+  {
+    question: `What other services do you provide?`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at urna pellentesque, vehicula urna quis, imperdiet libero. Aenean maximus sem id tempor accumsan. Integer nec ligula laoreet, blandit leo et, fermentum turpis.`,
+  },
+]
+
+
+export function Faq() {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null)
+
+  const toggleFaq = (index: number) => {
+    setActiveIndex(activeIndex === index ? null : index)
+  }
+
+  return (
+    <div className="max-w-md">
+      {faqs.map((faq, index) => (
+        <div key={index} className="">
+          <button
+            onClick={() => toggleFaq(index)}
+            className="w-full py-4 border-b border-black grid grid-cols-[1fr_auto] justify-between gap-x-4 cursor-pointer text-left"
+          >
+            <div>{index + 1}. {faq.question}</div>
+            <motion.div
+              animate={{ rotate: activeIndex === index ? 180 : 0 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            >
+              <ChevronDown className="text-neutral-600" />
+            </motion.div>
+          </button>
+          <AnimatePresence initial={false}>
+            {activeIndex === index && (
+              <motion.div
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: "auto", opacity: 1 }}
+                exit={{ height: 0, opacity: 0 }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className="overflow-hidden"
+              >
+                <div className="py-4 text-neutral-700">
+                  {faq.answer}
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+
+        </div>
+      ))}
+    </div>
+  )
+}
