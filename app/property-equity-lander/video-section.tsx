bcee62f01667eef404c0f3d14466911ee7323e25
minkaizen <martin.g.cao@gmail.com>
2025-12-10 14:33:52 +1100

    feat: add inquire button to video section

--- app/property-equity-lander/video-section.tsx
+++ app/property-equity-lander/video-section.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import Image from "next/image"
 
 export function VideoSection() {
@@ -14,6 +15,18 @@ export function VideoSection() {
             priority
           />
           <div className="aspect-video w-full bg-black rounded-lg"></div>
+          <Link href="#inquire_form" className="block w-max relative cursor-pointer">
+            <div className="px-3">Inquire</div>
+            <div
+              className="arrow absolute left-0 bottom-3"
+              style={{
+                ["--len" as any]: "100%",
+                ["--thick" as any]: "1px",
+                ["--head" as any]: "4px",
+                color: "white",
+              }}
+            />
+          </Link>
         </div>
       </div>
     </>
