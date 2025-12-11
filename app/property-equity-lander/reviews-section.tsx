d7913f4625432dcb376ea5ead6f34fe5df2d2d70
minkaizen <martin.g.cao@gmail.com>
2025-12-11 11:40:14 +1100

    feat: add elfsight widget

--- app/property-equity-lander/reviews-section.tsx
+++ app/property-equity-lander/reviews-section.tsx
@@ -2,7 +2,19 @@
 export function ReviewsSection() {
   return (
     <>
-
+      <div className="bg-neutral-100" style={{
+        fontWeight: 400,
+        fontSize: "14px",
+        lineHeight: "18px",
+        fontFamily:
+          'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+      }}>
+        <div className="px-5 py-8 lg:py-12 xl:py-18">
+          <script src="https://elfsightcdn.com/platform.js" async></script>
+          {/* <div className="elfsight-app-fe938bf5-818c-4ae9-bdff-f1992f81acc4" data-elfsight-app-lazy></div> */}
+          <div className="elfsight-app-5c48f7d4-cced-44cd-9ce7-7536b7604ffe" data-elfsight-app-lazy></div>
+        </div>
+      </div>
     </>
   )
 }
