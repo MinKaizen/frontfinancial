ff7a4b7cd4d00dc1730d2d4a504034d45b37fd47
minkaizen <martin.g.cao@gmail.com>
2025-12-12 11:30:38 +1100

    feat: add video to lander

--- app/property-equity-lander/video-section.tsx
+++ app/property-equity-lander/video-section.tsx
@@ -16,9 +16,9 @@ export function VideoSection() {
             priority
           />
 
-          <div className="aspect-video w-full bg-black rounded-lg relative overflow-hidden">
+          <div className="aspect-9/11 md:aspect-5/3 w-full bg-black rounded-lg relative overflow-hidden">
             <iframe
-              src="https://player.vimeo.com/video/1145449656?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1"
+              src="https://player.vimeo.com/video/1145800211?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1"
               frameBorder="0"
               allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
