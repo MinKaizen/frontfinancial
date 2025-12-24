eb7b185796810d893b3a51e3d8289b689a6a66cc
minkaizen <martin.g.cao@gmail.com>
2025-12-24 14:37:57 +1100

    feat: upodate vimeo link

--- app/property-equity-lander/video-section.tsx
+++ app/property-equity-lander/video-section.tsx
@@ -18,7 +18,7 @@ export function VideoSection() {
 
           <div className="aspect-9/11 md:aspect-5/3 w-full bg-black rounded-lg relative overflow-hidden">
             <iframe
-              src="https://player.vimeo.com/video/1145800211?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1"
+              src="https://player.vimeo.com/video/1149088447?h=ef40a6e3e3&title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479&autoplay=0"
               frameBorder="0"
               allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
