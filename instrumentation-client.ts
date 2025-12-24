788765eb036be08f8f27b3b2fea7ee83b3d4d04f
minkaizen <martin.g.cao@gmail.com>
2025-12-25 09:00:36 +1100

    feat: only load posthog on prod

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,7 +1,19 @@
 import posthog from 'posthog-js'
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  defaults: '2025-11-30'
-});
+const prodDomain = process.env.NEXT_PUBLIC_PROD_DOMAIN || 'frontfinancial.com.au'
+const prodDomains = [
+  prodDomain,
+  `www.${prodDomain}`,
+]
+const isProdDomain = typeof window !== 'undefined' && prodDomains.includes(window.location.hostname)
+
+if (isProdDomain) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    defaults: '2025-11-30'
+  });
+} else {
+  console.log(`Not on a prod domain. Skipping Posthog initialization...`)
+}
+
 
